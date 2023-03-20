ed51603e0be06e4647ff3280f4f2179af7780458
Lunora <itthings451@gmail.com>
2023-03-20 20:18:36 +0100

    Rework Js file - bind the js logic with ui and changment of DOM during different phase of the game

--- index.js
+++ index.js
@@ -29,47 +29,64 @@ let computerChoice = () => {
   }
 };
 
-let humanChoice = () => {
-  let weaponChoice = prompt(
-    "Choose between rock, paper and scissor for battle your ennemy !!, What is ur weapon :"
-  );
-  if (weaponChoice) {
-    weaponChoice = weaponChoice.toLowerCase();
-  }
-  if (
-    weaponChoice === "rock" ||
-    weaponChoice === "paper" ||
-    weaponChoice === "scissor"
-  ) {
-    return weaponChoice;
-  } else {
-    alert(
-      "Error : you need to enter a choice who is 'rock', 'paper' or 'scissor'"
-    );
-  }
-};
+// let humanChoice = () => {
+//   // let weaponChoice = prompt(
+//   //   "Choose between rock, paper and scissor for battle your ennemy !!, What is ur weapon :"
+//   // );
+//   if (weaponChoice) {
+//     weaponChoice = weaponChoice.toLowerCase();
+//   }
+//   if (
+//     weaponChoice === "rock" ||
+//     weaponChoice === "paper" ||
+//     weaponChoice === "scissor"
+//   ) {
+//     return weaponChoice;
+//   } else {
+//     // alert(
+//     //   "Error : you need to enter a choice who is 'rock', 'paper' or 'scissor'"
+//     // );
+//   }
+// };
 
 let winnerName = (computerChoice, humanChoice) => {
+  let winner = "";
+
   switch (computerChoice) {
     case "rock":
-      if (humanChoice == "rock") return "even";
-      if (humanChoice == "paper") return "human";
-      if (humanChoice == "scissor") return "computer";
+      if (humanChoice == "rock") winner = "even";
+      if (humanChoice == "paper") winner = "human";
+      if (humanChoice == "scissor") winner = "computer";
       break;
     case "paper":
-      if (humanChoice == "rock") return "computer";
-      if (humanChoice == "paper") return "even";
-      if (humanChoice == "scissor") return "human";
+      if (humanChoice == "rock") winner = "computer";
+      if (humanChoice == "paper") winner = "even";
+      if (humanChoice == "scissor") winner = "human";
       break;
     case "scissor":
-      if (humanChoice == "rock") return "human";
-      if (humanChoice == "paper") return "computer";
-      if (humanChoice == "scissor") return "even";
+      if (humanChoice == "rock") winner = "human";
+      if (humanChoice == "paper") winner = "computer";
+      if (humanChoice == "scissor") winner = "even";
       break;
 
     default:
       break;
   }
+
+  switch (winner) {
+    case "human":
+      humanCountVictory++;
+      break;
+    case "computer":
+      computerCountVictory++;
+      break;
+    case "even":
+      break;
+
+    default:
+      break;
+  }
+  return winner;
 };
 
 let roundMessageResult = (computer, human, winner) => {
@@ -78,51 +95,155 @@ let roundMessageResult = (computer, human, winner) => {
   } else if (winner === "computer") {
     return `you lose! ${computer} beat ${human}`;
   } else {
-    return `no success on stomping each other lad`;
+    return `no success on stomping each other lads`;
   }
 };
 
 let humanCountVictory = 0;
 let computerCountVictory = 0;
-let numberRound = 0;
-const roundNeededForWin = 5;
+let numberRound = 1;
+let numberOfGame = 0;
+const roundNeededForWin = 3;
 
-function game() {
-  while (
-    humanCountVictory < roundNeededForWin &&
-    computerCountVictory < roundNeededForWin
-  ) {
-    let iaWeapon = computerChoice();
-    let humWeapon = humanChoice();
-    let winner = winnerName(iaWeapon, humWeapon);
-    let messageOfRound = roundMessageResult(iaWeapon, humWeapon, winner);
+function round(humanWeapon = null) {
+  if (!humanWeapon) return;
 
-    switch (winner) {
-      case "human":
-        humanCountVictory++;
-        break;
-      case "computer":
-        computerCountVictory++;
-        break;
-      case "even":
-        break;
+  let iaWeapon = computerChoice();
+  let winner = winnerName(iaWeapon, humanWeapon);
+  let messageOfRound = roundMessageResult(iaWeapon, humanWeapon, winner);
 
-      default:
-        break;
-    }
-    console.log(
-      `round ${numberRound} : ${messageOfRound};  score : you:${humanCountVictory} computer:${computerCountVictory}`
-    );
-    numberRound++;
-  }
-  if (humanCountVictory == roundNeededForWin) alert("human is victorious");
-  if (computerCountVictory == roundNeededForWin)
-    alert("computer is victorious");
+  updateHistoric(messageOfRound);
+  updateScore(humanCountVictory, computerCountVictory);
+
+  numberRound++;
 }
 
-// let buttonLaunchGame = document.getElementById("start");
+function updateScore(scoreH = 0, scoreC = 0) {
+  // TODO maj le dom
+  let textC = scoreComputer.textContent;
+  let textH = scoreHuman.textContent;
 
-// buttonLaunchGame.addEventListener("click", () => {
-//   console.log(hey);
-//   game();
-// });
+  scoreHuman.textContent = textH.replace(
+    textH.substring(textH.length - 1, textH.length),
+    scoreH
+  );
+  scoreComputer.textContent = textC.replace(
+    textC.substring(textC.length - 1, textC.length),
+    scoreC
+  );
+
+  didItEnd();
+}
+function updateHistoric(messageOfRound) {
+  // TODO maj dans le dom
+  let pText = document.createElement("p");
+
+  pText.textContent = `round ${numberRound} : ${messageOfRound}.`;
+  historicBattle.appendChild(pText);
+}
+
+function didItEnd() {
+  if (humanCountVictory == roundNeededForWin) {
+    setPage3();
+    setResult(true);
+  }
+  if (computerCountVictory == roundNeededForWin) {
+    setPage3();
+    setResult(false);
+  }
+}
+
+function setResult(victory = null) {
+  if (victory == null) return;
+  let pResult = document.createElement("p");
+  if (victory) {
+    pResult.textContent =
+      "Congrats !! You win you're better than a random function.";
+    pResult.classList.add("victoryMessageStyle");
+  } else {
+    pResult.textContent =
+      "Sadly, you lose against computer, computer goes brrrr bepbep";
+    pResult.classList.add("loseMessageStyle");
+  }
+  play.insertBefore(pResult, buttonLaunchGame);
+  numberOfGame++;
+}
+
+/*
+ *
+ *
+ *
+ *
+ *
+ */
+
+let buttonLaunchGame = document.querySelector(".start");
+
+buttonLaunchGame.addEventListener("click", function (e) {
+  if (numberOfGame) reset();
+  //remise a zero de l'encart des scores
+  setPage2();
+});
+
+function reset() {
+  humanCountVictory = 0;
+  computerCountVictory = 0;
+  numberRound = 1;
+
+  let historyBattle = document.querySelectorAll(
+    ".historicBattle p:not(:first-child)"
+  );
+
+  historyBattle.forEach((line) => {
+    historicBattle.removeChild(line);
+  });
+  play.removeChild(document.querySelector(".containerStart p"));
+
+  updateScore();
+}
+
+// Start of Page
+
+const scoreHuman = document.querySelector(".scoreHuman");
+const scoreComputer = document.querySelector(".scoreComputer");
+const historicBattle = document.querySelector(".historicBattle");
+
+const score = document.querySelector(".positionScoreContainer");
+const weapon = document.querySelector(".positionChoiceWeaponContainer");
+const historic = document.querySelector(".positionHistoricBattle");
+const play = document.querySelector(".containerStart");
+
+const mainContent = document.querySelector(".main");
+
+// const rockChoice = document.querySelector(".rockChoice");
+// const paperChoice = document.querySelector(".paperChoice");
+// const scissorChoice = document.querySelector(".scissorChoice");
+
+const choiceWeapon = document.querySelectorAll(".choiceWeaponContainer > div");
+
+function setPage1() {
+  mainContent.removeChild(score);
+  mainContent.removeChild(weapon);
+  mainContent.removeChild(historic);
+  mainContent.appendChild(play);
+}
+
+function setPage2() {
+  mainContent.removeChild(play);
+  mainContent.appendChild(score);
+  mainContent.appendChild(weapon);
+  mainContent.appendChild(historic);
+}
+
+function setPage3() {
+  mainContent.insertBefore(play, score);
+  mainContent.removeChild(weapon);
+}
+
+choiceWeapon.forEach((weapon) => {
+  weapon.addEventListener("click", (e) => {
+    round(weapon.attributes[1].nodeValue);
+  });
+});
+
+setPage1();
